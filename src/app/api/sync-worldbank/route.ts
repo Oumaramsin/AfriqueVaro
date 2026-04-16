4aa59a88be16f6fe8f8c42f0c42e44f44694f8bb
oumaramsin <sidibeoumar94@gmail.com>
2026-04-17 01:20:49 +0200

    securisation des API

--- src/app/api/sync-worldbank/route.ts
+++ src/app/api/sync-worldbank/route.ts
@@ -5,8 +5,8 @@ import { NextResponse } from 'next/server'
 export async function POST() {
   try {
     const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
     )
 
     const { data: pays } = await supabase
